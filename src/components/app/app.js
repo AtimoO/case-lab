d654ad116c66595dc393621f4da54f6d040f91ec
A. Timokhin <timoha.job@gmail.com>
2022-06-28 17:05:46 +0300

    add: npm

---
+++ src/components/app/app.js
@@ -0,0 +1,13 @@
+import style from './app.module.css';
+
+function App() {
+  return (
+    <div className={style.app}>
+      <header className={style.app__header}>
+        <h1>Hello CaseLab</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
